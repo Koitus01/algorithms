918134d767c7f2fa1bb181b98905aa92155d9c2d
Артём Быков <a.bykov@sprinthost.ru>
2023-03-29 15:39:34 +0300

    solution with two nested loops

--- LongestSubstrWithoutRepeating.py
+++ LongestSubstrWithoutRepeating.py
@@ -1,36 +1,30 @@
 # https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
 class Solution:
     def lengthOfLongestSubstring(self, s: str):
-        previous_values = []
         max_value = 0
-        local_value = 0
         s_list = list(s)
-        s_len = len(s)
 
         for index, char in enumerate(s_list):
-            if index + 1 < s_len and s_list[index + 1] in previous_values and char != s_list[index - 1]:
-                local_value += 1
-                if max_value < local_value:
-                    max_value = local_value
-                local_value = 0
-                previous_values = []
+            local_max = 1
+            previous_char = []
+            for sub_char in s_list[1 + index:]:
+                if sub_char is not char and sub_char not in previous_char:
+                    local_max += 1
+                else:
+                    break
+                previous_char.append(sub_char)
 
-            if char not in previous_values:
-                local_value += 1
-                if max_value < local_value:
-                    max_value = local_value
-                previous_values.append(char)
-            else:
-                local_value = 1
+            if local_max > max_value:
+                max_value = local_max
 
         return max_value
 
 
 substrs = [
-    'anviaj', # 4
+    'abcabcbb',  # 3
+    'anviaj',  # 5
     'dvdf',  # 3
     'abcapaik',  # 4
-    'abcabcbb',  # 3
     'bbbbb',  # 1
     'pwwkew',  # 3
     'aab'  # 2
