014b7d53b45e08dfb8ed26aad2cac0cdaef760ba
Артём Быков <a.bykov@sprinthost.ru>
2023-03-29 20:40:14 +0300

    return s[0] if result is empty

--- LongestPalindrom.py
+++ LongestPalindrom.py
@@ -1,5 +1,11 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
+        # if len(s) == 1:
+        #     return s
+        #
+        # if len(s) == 2 and s[0] != s[1]:
+        #     return s[0]
+
         s_list = list(s)
         result = ''
 
@@ -8,35 +14,27 @@ class Solution:
             for sub_char in s_list[1 + index:]:
                 palindrome_str += sub_char
                 palindrome_length = len(palindrome_str)
-                if palindrome_length == 2 and palindrome_str[0] == palindrome_str[1] and len(result) < 2:
-                    result = palindrome_str
-                    break
-
-                if palindrome_length == 3 and palindrome_str[0] == palindrome_str[-1] and len(result) < 3:
-                    result = palindrome_str
-                    break
 
                 middle_of_palindrome = int(palindrome_length / 2)
                 if palindrome_length % 2 == 1:
-                    if palindrome_str[middle_of_palindrome][::-1] is palindrome_str[0:middle_of_palindrome]:
-                        if len(result) < palindrome_length:
-                            result = palindrome_str
-                            break
-                    # odd
-                #else:
-                    #break
-                    # no odd
+                    begin_of_palindrome = palindrome_str[0:middle_of_palindrome]
+                    end_of_palindrome = palindrome_str[middle_of_palindrome + 1:palindrome_length][::-1]
+                else:
+                    half_of_palindrome_length = int(palindrome_length / 2)
+                    begin_of_palindrome = palindrome_str[0:half_of_palindrome_length]
+                    end_of_palindrome = palindrome_str[half_of_palindrome_length:palindrome_length][::-1]
+
+                if begin_of_palindrome == end_of_palindrome:
+                    if len(result) < palindrome_length:
+                        result = palindrome_str
+
+        if result == '':
+            return s[0]
 
         return result
 
 
-
-# s = 'canac'
-# s_len = len(s)
-# start_position = int(s_len / 2)
-# print(s[-start_position:])
-
-strings = ['cbbd', 'abac', 'cabala', 'canac', 'pi234pi', 'aaaaapipiaaaaa']
+strings = ['abcda', 'ccc', 'pgaagx', 'ac', 'a', 'cbbd', 'abac', 'cabala', 'canac', 'pi234pi', 'aaaaapipiaaaaa']
 
 for i in strings:
     print(Solution().longestPalindrome(s=i))
