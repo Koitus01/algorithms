f055e5d1f2ba314b0de9d0aaf93ba345f3dde1cd
Артём Быков <a.bykov@sprinthost.ru>
2023-04-10 17:58:47 +0300

    correct, but output result may have incorrect order

--- MergeStringIntervals.py
+++ MergeStringIntervals.py
@@ -1,34 +1,46 @@
 class Solution:
     def merge_intervals(self, intervals: list):
-        local_min = 0
-        local_max = 0
+        global_min = 0
+        global_max = 0
         previous_ranges = []
         result = []
-        enumerate_intervals = enumerate(intervals)
 
         for interval in intervals:
             [i_min_value, i_max_value] = self.split_str(interval)
-            str_interval = ''
-
             for r in previous_ranges:
-                [r_min_value, r_max_value] = self.split_str(r)
-                r_range = range(r_min_value, r_max_value + 1)
+                r_min = r[0]
+                r_max = r[1]
+                if r_min <= i_min_value <= r_max:
+                    local_min = min(i_min_value, r_min)
+                    local_max = max(i_max_value, r_max)
+                    global_min = min(local_min, global_min)
+                    global_max = max(local_max, global_max)
+                    for index, re in enumerate(result):
+                        [re_min, re_max] = self.split_str(re)
+                        if local_min == re_min and local_max == re_max:
+                            del result[index]
+                            continue
+                        if local_min <= re_min <= local_max:
+                            del result[index]
+                            continue
 
-                if i_min_value not in r_range:
-                    if r_max_value < r_min_value:
-                        str_interval = self.union_str(r_min_value, r_max_value)
-                    else:
-                        str_interval = self.union_str(i_min_value, i_max_value)
-                    continue
+                    united_str = self.union_str(local_min, local_max)
+                    result.append(united_str)
 
-                str_interval = self.union_str(min(i_min_value, r_min_value), max(i_max_value, r_max_value))
-                result.append(str_interval)
+                if r_min > global_max:
+                    united_str = self.union_str(r_min, r_max)
+                    result.append(united_str)
+                    global_max = r_min
+                #print(r, r, global_min, global_max)
 
-            if interval not in previous_ranges:
-                previous_ranges.append(interval)
-            if str_interval != '' and str_interval not in result:
-                result.append(str_interval)
 
+                    #if len(result) > 1
+
+            if i_min_value > global_max:
+                united_str = self.union_str(i_min_value, i_max_value)
+                result.append(united_str)
+                global_max = i_min_value
+            previous_ranges.append([i_min_value, i_max_value])
         return result
 
     def split_str(self, interval: str):
@@ -47,7 +59,8 @@ cases = [
     ['1-3', '3-5', '6-7', '2-4'],
     ['1-5', '1-10', '3-20'],
     ['1-5', '2-4', '7-9'],
-    ['2-5', '7-10']
+    ['2-5', '7-10'],
+    ['1-3', '2-6', '8-10', '15-18']
 ]
 
 for case in cases:
